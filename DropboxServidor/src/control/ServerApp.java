4c283c7a274041c0473320fc4d08a4c4dd9b475f
giovaninb <giovanib@ufcspa.edu.br>
2017-06-09 15:11:16 -0300

    Inicio do projeto. Separação em model-view-control.

---
+++ DropboxServidor/src/control/ServerApp.java
@@ -0,0 +1,24 @@
+package control;
+
+import java.awt.EventQueue;
+import view.JanelaPrincipal;
+
+public class ServerApp {
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaPrincipal frame = new JanelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
