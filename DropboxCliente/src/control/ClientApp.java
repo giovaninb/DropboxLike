e20b237951a35863a62959f608985d0f5043df69
giovaninb <giovanib@ufcspa.edu.br>
2017-06-09 18:42:28 -0300

    Agora vai

---
+++ DropboxCliente/src/control/ClientApp.java
@@ -0,0 +1,27 @@
+package control;
+
+import java.awt.EventQueue;
+import view.JanelaPrincipal;
+
+public class ClientApp {
+	
+	/**
+	 * Launch the application.
+	 */
+	
+	public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						JanelaPrincipal frame = new JanelaPrincipal();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+	}
+
+
---
+++ DropboxServidor/src/view/JanelaPrincipal.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class JanelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public JanelaPrincipal() {
+		String nomeAplicacao = "Server App";
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setTitle(nomeAplicacao);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
