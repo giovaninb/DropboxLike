808e92465637acb3eba9568b00210cbf006816d7
giovaninb <giovanib@ufcspa.edu.br>
2017-06-17 03:52:58 -0300

    GUI Pronta
    Design Pattern da GUI está pronto. Começar a implementar Modelo e Controle.

--- DropboxCliente/src/view/JanelaPrincipal.java
+++ DropboxCliente/src/view/JanelaPrincipal.java
@@ -16,6 +16,9 @@ import java.awt.Font;
 import javax.swing.JTextArea;
 import javax.swing.JProgressBar;
 import java.awt.FlowLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class JanelaPrincipal extends JFrame {
 
@@ -29,8 +32,17 @@ public class JanelaPrincipal extends JFrame {
 		String nomeAplicacao = "Client App";
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 400);
 		setTitle(nomeAplicacao);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mnMenu.add(mntmSair);
 		painelCentral = new JPanel();
 		painelCentral.setBorder(new EmptyBorder(5, 5, 5, 5));
 		painelCentral.setLayout(new BorderLayout(0, 0));
@@ -58,24 +70,52 @@ public class JanelaPrincipal extends JFrame {
 		JPanel painelCentro = new JPanel();
 		painelCentro.setBorder(new EmptyBorder(10, 10, 10, 10));
 		painelCentral.add(painelCentro, BorderLayout.CENTER);
-		painelCentro.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		painelCentro.setLayout(new GridLayout(1, 2, 5, 5));
 		
 		JPanel panelSelectedImages = new JPanel();
 		painelCentro.add(panelSelectedImages);
-		panelSelectedImages.setLayout(new BorderLayout(0, 0));
+		panelSelectedImages.setLayout(new BorderLayout(5, 5));
 		
 		JLabel lblTitleSelectedImages = new JLabel("Imagens Selecionadas");
+		lblTitleSelectedImages.setHorizontalAlignment(SwingConstants.CENTER);
 		panelSelectedImages.add(lblTitleSelectedImages, BorderLayout.NORTH);
 		
 		JTextArea textArea = new JTextArea();
+		textArea.setTabSize(10);
+		textArea.setEditable(false);
 		panelSelectedImages.add(textArea, BorderLayout.CENTER);
 		
-//		JPanel panelSendImages = new JPanel();
-//		painelCentro.add(panelSendImages);
-//		panelSendImages.setLayout(new BorderLayout(0, 0));
-//		
-//		JLabel lblTitleSendImages = new JLabel("Imagens Enviadas");
-//		panelSendImages.add(lblTitleSendImages);
+		JPanel panelSendImages = new JPanel();
+		painelCentro.add(panelSendImages);
+		panelSendImages.setLayout(new BorderLayout(5, 5));
+		
+		JLabel lblTitleSendImages = new JLabel("Imagens Enviadas");
+		lblTitleSendImages.setHorizontalAlignment(SwingConstants.CENTER);
+		panelSendImages.add(lblTitleSendImages, BorderLayout.NORTH);
+		
+		JTextArea textArea2 = new JTextArea();
+		panelSendImages.add(textArea2, BorderLayout.CENTER);
+		textArea2.setTabSize(10);
+		textArea2.setEditable(false);
+		
+		JPanel panelBaixo = new JPanel();
+		painelCentral.add(panelBaixo, BorderLayout.SOUTH);
+		panelBaixo.setLayout(new GridLayout(3, 2, 10, 10));
+		
+		JProgressBar progressBar = new JProgressBar();
+		panelBaixo.add(progressBar);
+		
+		JButton btnEnviarArquivos = new JButton("Enviar Arquivos");
+		panelBaixo.add(btnEnviarArquivos);
+		
+		JLabel lblCronometro = new JLabel("Cronometro");
+		lblCronometro.setHorizontalAlignment(SwingConstants.CENTER);
+		panelBaixo.add(lblCronometro);
+		
+		JButton btnRemoverArquivos = new JButton("Remover Arquivos");
+		panelBaixo.add(btnRemoverArquivos);
+		
+		
 		
 	}
 
--- DropboxServidor/src/view/JanelaPrincipal.java
+++ DropboxServidor/src/view/JanelaPrincipal.java
@@ -1,37 +1,91 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import javax.swing.SpringLayout;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
 
 public class JanelaPrincipal extends JFrame {
 
-	private JPanel painelCentral;
-
+	private JDesktopPane contentPane;
+	private int cont = 0;
 	/**
 	 * Create the frame.
 	 */
 	public JanelaPrincipal() {
+		BorderLayout borderLayout = (BorderLayout) getContentPane().getLayout();
+		borderLayout.setVgap(5);
+		borderLayout.setHgap(5);
+		
+		contentPane = new JDesktopPane();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		getContentPane().add(contentPane);
+		contentPane.setLayout(new GridLayout(1, 3, 5, 5));
+		
 		String nomeAplicacao = "Server App";
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(900, 100, 945, 600);
 		setTitle(nomeAplicacao);
-		painelCentral = new JPanel();
-		painelCentral.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(painelCentral);
-		painelCentral.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JInternalFrame internalFrame = new JInternalFrame("Client #");
-		painelCentral.add(internalFrame);
-		internalFrame.setVisible(true);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmConecta = new JMenuItem("Conecta Cliente");
+		mntmConecta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JInternalFrame interna = new JInternalFrame("InternalFrame", true, true, true);
+				interna.setVisible(true);
+				interna.setBounds(cont, cont, 400, 400);
+				cont = cont +50;
+				contentPane.add(interna);
+				
+				JTabbedPane tabbed = new JTabbedPane();
+
+				JPanel p1 = new JPanel();
+				JLabel l1 = new JLabel("Painel 1");
+				p1.add(l1);
+				tabbed.add("tab1", p1);
+
+				JPanel p2 = new JPanel();
+				JLabel l2 = new JLabel("Painel 2");
+				p2.add(l2);
+				tabbed.add("tab2", p2);
+
+				JPanel p3 = new JPanel();
+				JLabel l3 = new JLabel("Painel 3");
+				p3.add(l3);
+				tabbed.add("tab3", p3);
+
+				interna.getContentPane().add(tabbed);
+				tabbed.setVisible(true);
+				validate();
+			}
+		});
+		mnMenu.add(mntmConecta);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mnMenu.add(mntmSair);
 	}
 
 }
